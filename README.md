30a16321e7b8caacb1ea5238c64cf4099d790579
Sikkink <Drew.Sikkink@mail.house.gov>
2024-04-02 09:46:13 -0400

    v0.4 added case studies

--- README.md
+++ README.md
@@ -9,35 +9,38 @@ The main purpose of having this repo is to use Streamlit Community Cloud to host
 - 0.3 - for the drop down menu, only include "agencies" that have outlays recorded for all three sources
 
 ## Random Notes
+
+(0.3) Playfair Display font is not available natively for the app. Would have to add it to the directory, which shouldn't be too much trouble?
+
 (0.3) There are some agencies that don't really have significant differences (or are less than before):
-    Department of Justice
-    Department of Health and Human Services
-    Deparment of Justice
-    Department of Labor (has some funky stuff tho)
-    Department of State (less now)
-    Judicial Branch
-    NASA
-    SBA (shows covid spike tho)
+Department of Justice  
+Department of Health and Human Services  
+Deparment of Justice  
+Department of Labor (has some funky stuff tho)  
+Department of State (less now)  
+Judicial Branch  
+NASA  
+SBA (shows covid spike tho)  
     
 (0.3) What is going on with 'Other Defense--Civil Programs'?
 
-Agencies in the CBO24 that aren't in the CBO21:
-    400 Years of African-American History Commission
-    Southeast Crescent Regional Commission
-    Southwest Border Regional Commission
+Agencies in the CBO24 that aren't in the CBO21:  
+400 Years of African-American History Commission   
+Southeast Crescent Regional Commission   
+Southwest Border Regional Commission
 
-"Agencies" in the OMB that aren't in CBO24:
-    United States Enrichment Corporation Fund
-    Social Security Administration (On-Budget)
-    Social Security Administration (Off-Budget)
-    Other Independent Agencies (On-Budget)
-    Other Independent Agencies (Off-Budget)
-    Undistributed Offsetting Receipts
-    (On-budget)
-    (Off-budget)
-    Total outlays
+"Agencies" in the OMB that aren't in CBO24:  
+United States Enrichment Corporation Fund  
+Social Security Administration (On-Budget)  
+Social Security Administration (Off-Budget)  
+Other Independent Agencies (On-Budget)  
+Other Independent Agencies (Off-Budget)  
+Undistributed Offsetting Receipts  
+(On-budget)  
+(Off-budget)  
+Total outlays  
 
 Important to note that in the long format df, only the OMB data is actual outlays. The CBO21 and CBO24 are projections. NaN values fill the gaps. 
-    on that note, to match Jame's charts, I added values to the outlays in the year previous to the CBO projections from OMB (so the actual outlays):
-    so CBO24 has outlays recorded for 2023 but these are taken from OMB's data
-    CBO21 has outlays recorded for 2020 but these are taken from OMB's data
+on that note, to match Jame's charts, I added values to the outlays in the year previous to the CBO projections from OMB (so the actual outlays):
+so CBO24 has outlays recorded for 2023 but these are taken from OMB's data
+CBO21 has outlays recorded for 2020 but these are taken from OMB's data
--- produce_charts/streamlit_app.py
+++ produce_charts/streamlit_app.py
@@ -6,7 +6,7 @@ from matplotlib import ticker
 import streamlit as st
 
 #notes: Playfair Display font is not available in the streamlit app
-#            what if we changed the legend labels to be something like "Projection Before Biden" and "Projection After Biden"?
+# hat if we changed the legend labels to be something like "Projection Before Biden" and "Projection After Biden"?
 
 
 ### STEP ONE: load data ###
@@ -41,12 +41,65 @@ def linechart_maker(agency):
     sns.despine(offset=15, left=True)
     st.pyplot(plt.gcf())
 
+def barchart_maker(agency):
+    plt.figure(figsize=(12, 5.3))
+    sns.set_style('white')
+    barchart_df = cs_increase_programs[cs_increase_programs['Agency'] == agency]
+    barchart_df = pd.melt(barchart_df, id_vars='Title', value_vars=['22-31', '25-34'], var_name='10-year Projections', value_name='Value').sort_values(by=["10-year Projections"], ascending=True)
+    barchart_df['Value'] = barchart_df['Value'] / 1000
+    barchart = sns.barplot(data=barchart_df, x='Title', y='Value', hue='10-year Projections', palette={'22-31': '#84AE95', '25-34': '#004647'})
+    plt.title(f'{agency} \n 10-Year Outlay Increases by Program', weight='bold', fontsize=16)
+    plt.ylabel('Outlay Increase \n (in billions)')
+    plt.xlabel('')
+    sns.despine()
+    plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f'${x:,.0f}'))
+    
+    legend = barchart.get_legend() # Get the current legend
+    legend.set_title('')
+    for t, l in zip(legend.texts, ('2021 Projection', '2024 Projection')): # Rename the legend labels
+        t.set_text(l)
+    st.pyplot(plt.gcf())
+
 ### STEP THREE: Streamlit app ###
 
-#Opening
-st.title("Biden's Big Government")
-st.write(f"""President Biden has presided over a flood of government spending. His executive agencies have been the beneficiaries of this involuntary gift from taxpayers. 
-         Explore the data below to see how the projected outlays of {len(df['agency'].unique())} agencies have changed since Biden took office.""")
-#Chart maker
-agency = st.selectbox('Select an agency or start typing:', df['agency'].unique())
-linechart_maker(agency)
+#sidebar
+page = st.sidebar.radio("Go to page", ["Home", "Case Studies" "About"])
+rd_file_path = "README.md"
+
+if page == "Home":
+    st.title("Biden's Big Government")
+    st.write(f"""President Biden has presided over a flood of government spending. His executive agencies have been the beneficiaries of this involuntary gift from taxpayers. 
+             Explore the data below to see how the projected outlays of {len(df['agency'].unique())} agencies have changed since Biden took office.""")
+    #Chart maker
+    agency = st.selectbox('Select an agency or start typing:', df['agency'].unique())
+    linechart_maker(agency)
+
+if page == "About":
+     with open(rd_file_path, "r") as f:
+        readme_text = f.read()
+        st.markdown(readme_text)
+
+if page == "Case Studies":
+    st.header("Case Studies")
+    st.write("Take a deeper dive into the data behind the increased spending projections for six key agencies.")
+    case_studies = ['Department of Agriculture', 'Department of Commerce', 'Department of Labor', "Department of the Treasury",
+                     "Department of Transportation", "Environmental Protection Agency"]
+    agency = st.selectbox('Select an agency:', case_studies)
+
+    cs_increase = pd.read_excel('output/cbo_projection_changes.xlsx', sheet_name="case studies increases by agency")
+    cs_increase = cs_increase[cs_increase['Agency'] == agency]
+    st.subheader(f"{agency}")
+    st.markdown("##### Changes in projected outlays (in billions)")
+    cs_21 = f"${int(round(cs_increase['22-31'].values[0] / 1000, 0)):,}"
+    cs_24 = f"${int(round(cs_increase['25-34'].values[0] / 1000,0)):,}"
+    cs_pct = f"{round(cs_increase['percent_change'].values[0])}%"
+
+    col1, col2, col3 = st.columns(3)
+    col1.metric(label="10 Year Projection in 2021", value=cs_21)
+    col2.metric(label="10 Year Projection in 2024", value=cs_24)
+    col3.metric(label="Increase in 10 Year Projection", value=cs_pct)
+
+    st.write('\n')
+    st.markdown("##### Program increases behind the change:")
+    cs_increase_programs = pd.read_excel('output\program_increases.xlsx', sheet_name="case studies")
+    barchart_maker(agency)
